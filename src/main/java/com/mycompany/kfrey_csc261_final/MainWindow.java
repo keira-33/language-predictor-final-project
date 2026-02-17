49d61458d641bbaef4b80707d85b20f0f696b366
keira-33 <kfrey@mail.endicott.edu>
2026-02-17 16:52:04 -0500

    -First commit

---
+++ src/main/java/com/mycompany/kfrey_csc261_final/MainWindow.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.kfrey_csc261_final;
+
+/**
+ *
+ * @author kfrey
+ */
+public class MainWindow {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
